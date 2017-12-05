a87f8a8f76c33d5187a65308fbeac06ba6ea3ca8
DeividZavala <crusedmoss@gmail.com>
2017-12-05 15:04:12 -0600

    bindings unidireccionales

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,11 +4,42 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
-    <div class="app">
-      Hello!
-    </div>
+
+  <div class="app">
+    {{ title }}
+  </div>
+
+  <div>
+	<button (click)="handleClick()">Picale Papá</button>
+	<input
+	[value]="name"
+	(input)="handleInput($event)"
+	(blur)="handleBlur($event)"
+	/>
+	{{name}}
+  </div>
+
   `
 })
-export class AppComponent {
+export class AppComponent{
+	title: string;
+	name: string;
+
+	constructor(){
+		this.title = 'Testing angular 5';
+		this.name = 'David';
+	}
+
+	handleInput(event: any){
+		this.name = event.target.value;
+	}
+
+	handleBlur(event: any){
+		this.name = event.target.value;
+	}
+
+	handleClick(){
+		this.name = 'perro';
+	}
 
 }
